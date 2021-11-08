42a651d5472261c81fe955dd8d5ec88361a1efb1
Tanuj Sutaria <tanujsutaria@Tanujs-MacBook-Pro-2.local>
2021-11-07 17:48:36 -0800

    Setting up base Oauth impl

--- playlist-flask-backend/api/__init__.py
+++ playlist-flask-backend/api/__init__.py
@@ -1,4 +1,10 @@
+from urllib.parse import scheme_chars
 from flask import Flask
 import spotipy
 from spotipy import oauth2
+from Meta import auth_constants
+
+sp_auth = oauth2.SpotifyOAuth( auth_constants.SPOTIFY_CLIENT_ID, 
+auth_constants.SPOTIFY_CLIENT_SECRET, auth_constants.SPOTIFY_REDIRECT_URI,
+scope=auth_constants.SCOPE, cache_path=auth_constants.CACHE )
 
